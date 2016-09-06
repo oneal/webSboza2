cbc84064e4d654d4df141ccef3db68d475305a8c
Antonio Molina Cruz <oneal152@gmail.com>
2016-09-07 00:50:13 +0200

    added courses

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -27,7 +27,56 @@ class DefaultController extends Controller
         );
     }
 
+    /**
+     * @Route("/master-disenio-editorial-publicitario-disenio-web", name="masterEditorialDesign")
+     */
 
+    public function masterEditorialDesignAction(Request $request){
+        $form = $this->contactHome($request);
+
+        if($form->isSubmitted()){
+            return $this->redirectToRoute('thanks');
+        }
+
+        return $this->render('desktop/courses/master_editorial_design.html.twig', array(
+                'form' => $form->createView()
+            )
+        );
+    }
+
+    /**
+     * @Route("/curso-superior-disenio-grafico", name="superiorGgraphicDesign")
+     */
+
+    public function superiorGgraphicDesignAction(Request $request){
+        $form = $this->contactHome($request);
+
+        if($form->isSubmitted()){
+            return $this->redirectToRoute('thanks');
+        }
+
+        return $this->render('desktop/courses/superior_graphic_design.html.twig', array(
+                'form' => $form->createView()
+            )
+        );
+    }
+
+    /**
+     * @Route("/curso-motion-graphics", name="cursoMotionGraphics")
+     */
+
+    public function cursoMotionGraphicsAction(Request $request){
+        $form = $this->contactHome($request);
+
+        if($form->isSubmitted()){
+            return $this->redirectToRoute('thanks');
+        }
+
+        return $this->render('desktop/courses/course_motion_graphics.html.twig', array(
+                'form' => $form->createView()
+            )
+        );
+    }
 
     /**
      * @Route("/curso-superior-de-ilustracion-digital-y-tradicional", name="courseIlustrationDigitalTraditional")
@@ -45,6 +94,41 @@ class DefaultController extends Controller
             )
         );
     }
+
+    /**
+     * @Route("/curso-superior-fotografia", name="superiorPhotografic")
+     */
+
+    public function superiorPhotograficAction(Request $request){
+        $form = $this->contactHome($request);
+
+        if($form->isSubmitted()){
+            return $this->redirectToRoute('thanks');
+        }
+
+        return $this->render('desktop/courses/superior_photography.html.twig', array(
+                'form' => $form->createView()
+            )
+        );
+    }
+
+    /**
+     * @Route("/curso-caracterizacon-fx", name="courseCharacterizationFx")
+     */
+
+    public function courseCharacterizationFxAction(Request $request){
+        $form = $this->contactHome($request);
+
+        if($form->isSubmitted()){
+            return $this->redirectToRoute('thanks');
+        }
+
+        return $this->render('desktop/courses/course_characterization_fx.html.twig', array(
+                'form' => $form->createView()
+            )
+        );
+    }
+
 
     /**
      * @Route("/gracias", name="thanks")
